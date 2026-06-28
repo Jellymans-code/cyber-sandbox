c244c5c14ed638545d2a2d7154929e6899ac7ae6
Jellymans-code <khereis200@gmail.com>
2026-06-28 16:25:30 -0600

    Implement core eBPF telemetry engine with Docker isolation

--- backend/sandbox.py
+++ backend/sandbox.py
@@ -1,8 +1,10 @@
+from bcc import BPF
 import docker
 import time
 import sys
 
 def main():
+
     print("Connecting to Docker engine...")
     try:
         client = docker.from_env()
@@ -10,11 +12,12 @@ def main():
         print(f"Failed to connect to Docker: {e}")
         sys.exit(1)
 
-    print("Configuring isolated sandbox environment...")
+    print("Booting isolated sandbox environment...")
     try:
-        container =  client.containers.create(
+        container =  client.containers.run(
             image = "alpine",
-            command = ["sleep", "5"],
+            command = ["sh", "-c", "sleep 3; cat /etc/passwd; sleep 3"],
+            detach = True,
             network_mode = "none",
             mem_limit= "64m",
             nano_cpus = 500000000
@@ -22,28 +25,57 @@ def main():
     except Exception as e:
         print(f"Failed to create container: {e}")
         sys.exit(1)
-
-    def cleanup():
-        print("Cleaning up sandbox environment...")
-        try:
-            container.remove(force = True)
-            print("Sandbox demolished successfully")
-        except Exception:
-            pass
     
-    print("Booting up the container...")
-    container.start()
-    print(f"Container {container.short_id} is now alive")
-    print("Waiting for sandbox execution to complete...")
-    while True:
+    print(f"Container {container.short_id} is now running")
+
+    container.reload()
+    target_pid = container.attrs['State']['Pid']
+
+    ebpf_source_code = f"""
+    #include <linux/sched.h>
+
+    struct data_t {{
+        u32 pid;
+        char command[TASK_COMM_LEN];
+    }};
+
+    BPF_PERF_OUTPUT(events);
+
+    int detect_process_execution(void *content) {{
+        u32 pid = bpf_get_current_pid_tgid() >> 32;
+        
+        struct data_t data = {{}};
+        data.pid = pid;
+        bpf_get_current_comm(&data.command, sizeof(data.command));
+
+        events.perf_submit(content, &data, sizeof(data));
+        return 0;
+    }}
+    """
+    print("Loading eBPF monitor...")
+    try:
+        bpf_monitor = BPF(text = ebpf_source_code)
+        bpf_monitor.attach_tracepoint(tp = "syscalls:sys_enter_execve", fn_name = "detect_process_execution")
+    except Exception as e:
+        print(f"Failed to load eBPF progran. Error: {e}")
+        sys.exit(1)
+    print("eBPF monitor is live")
+
+    def print_event(cpu, data, size):
+        event = bpf_monitor["events"].event(data)
+        cmd = event.command.decode('utf-8')
+        if cmd in ["sh", "sleep", "cat"]:
+            print(f"[Sandbox Alert] PID: {event.pid} executed command: {cmd}")
+    bpf_monitor["events"].open_perf_buffer(print_event)
+    
+    while container.status != "exited":
         container.reload()
-        if container.status == "exited":
-            break
-        time.sleep(0.5)
+        bpf_monitor.perf_buffer_poll()
 
-    print("Container finished its lifecycle and exited safely")
 
-    cleanup()
+    print("Cleaning up sandbox...")
+    container.remove(force = True)
+    print("Sandbox shut down safely")
 
 if __name__ == "__main__":
     main()
